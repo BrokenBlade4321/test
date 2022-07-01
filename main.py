af2d5e605498c289edc16517dd68c4210d8d6242
BrokenBlade4321 <4over3piroga@gmail.com>
2022-07-01 14:13:05 +0300

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+def check_is_string(text) -> bool :
+    if isinstance(text, (int, float)):
+        return False
+    if not text.isalpha():
+        return False
+    return True
+
+
+def check_is_decimal(text) -> bool:
+    if isinstance(text, int):
+        return text > 0
+    if isinstance(text, float):
+        return False
+    return text.isdecimal()
+
+
+def check_excel_document_is_right(data) -> bool:
+    for row in data.iloc[0:, 0:].itertuples():
+        for i in range(2, len(row)):
+            if not CONST[row[1]](row[i]):
+                return False
+    return True
+
+def main(file):
+    df = pd.read_excel(f'{file}.xlsx')
+    if set(df.iloc[0:, 0]) - CONST.keys():
+        print("неверного формата")
+    elif check_excel_document_is_right(df):
+        print("верного формата")
+    else:
+        print("неверного формата")
+CONST = {'Имя': check_is_string, 'Фамилия': check_is_string, 'Возраст': check_is_decimal}
+file = "data"
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    df = pd.read_excel(f'{file}.xlsx')
+    if set(df.iloc[0:, 0]) - CONST.keys():
+        print("неверного формата")
+    elif check_excel_document_is_right(df):
+        print("верного формата")
+    else:
+        print("неверного формата")
